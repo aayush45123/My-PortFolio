362a351b1278f022e4efcf5c3b3434cbb0c6e1ea
Aayush Bharda <aayushbharda999@gmail.com>
2026-08-26 19:54:59 +0530

    solved bugs

--- src/component/Background/Background.jsx
+++ src/component/Background/Background.jsx
@@ -1,25 +1,13 @@
 import React from "react";
 import styles from "./Background.module.css";
-import darkBg from "../../assets/darkthemed.png";
-import lightBg from "../../assets/lightthemed.jpg";
 
 const Background = () => {
   return (
     <div className={styles.bgContainer} aria-hidden="true">
-      {/* Light theme background layer */}
-      <div
-        className={`${styles.bgLayer} ${styles.lightLayer}`}
-        style={{ backgroundImage: `url(${lightBg})` }}
-      />
-
-      {/* Dark theme background layer */}
-      <div
-        className={`${styles.bgLayer} ${styles.darkLayer}`}
-        style={{ backgroundImage: `url(${darkBg})` }}
-      />
-
-      {/* Subtle overlay for optimal content readability */}
-      <div className={styles.overlay} />
+      {/* Top ambient soft radial glow */}
+      <div className={styles.ambientGlowTop} />
+      {/* Bottom ambient accent glow */}
+      <div className={styles.ambientGlowBottom} />
     </div>
   );
 };
