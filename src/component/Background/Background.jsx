3622daa89ce1b31ec27452dfa75baaf8c78c45d1
Aayush Bharda <aayushbharda999@gmail.com>
2026-08-26 20:35:34 +0530

    solved bugs

--- src/component/Background/Background.jsx
+++ src/component/Background/Background.jsx
@@ -1,10 +1,9 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import styles from "./Background.module.css";
 
 const Background = () => {
   return (
     <div className={styles.pageStack} aria-hidden="true">
-      {/* Stacked paper layers for depth */}
       <div className={styles.paperLayer3} />
       <div className={styles.paperLayer2} />
       <div className={styles.paperLayer1} />
